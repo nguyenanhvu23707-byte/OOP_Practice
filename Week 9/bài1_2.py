b62e47eab495e100d9bb721043dc242e51ac5b4f
nguyenanhvu23707-byte <nguyenanhvu23707@gmail.com>
2026-05-15 23:21:36 +0700

    Update bài1_2.py

--- Week 9/bài1_2.py
+++ Week 9/bài1_2.py
@@ -1,1 +1,13 @@
+class PointTest:
+    def testCase(self):
+        p1 = Point()
+        print(p1)
 
+        p2 = Point()
+        p2.read()
+        print(p2)
+
+        p2.move(1, 1)
+        print(p2)
+
+        print(p2.distance())
