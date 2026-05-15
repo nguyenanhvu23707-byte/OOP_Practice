64d269b03f9ad252b1e24ff552458b60e7f7e828
nguyenanhvu23707-byte <nguyenanhvu23707@gmail.com>
2026-05-15 23:06:24 +0700

    Create bai3_1.py

---
+++ Week 9/bai3_1.py
@@ -0,0 +1,59 @@
+import math
+
+class Point:
+    def __init__(self, x=0, y=1):
+        self.__x = x
+        self.__y = y
+
+    def read(self):
+        x, y = map(int, input().split())
+        self.__x, self.__y = x, y
+
+    def __str__(self):
+        return f"({self.__x}, {self.__y})"
+
+    def move(self, dx, dy):
+        self.__x += dx
+        self.__y += dy
+
+    def getX(self):
+        return self.__x
+
+    def getY(self):
+        return self.__y
+
+    def setXY(self, x, y):
+        self.__x, self.__y = x, y
+
+    def distance(self, P=None):
+        if P is None:
+            return math.sqrt(self.__x**2 + self.__y**2)
+        else:
+            return math.sqrt((self.__x - P.getX())**2 + (self.__y - P.getY())**2)
+
+
+class ColorPoint(Point):
+    def __init__(self, *args):
+        if len(args) == 0:  
+            super().__init__()
+            self.__color = "xanh"
+        elif len(args) == 3: 
+            super().__init__(args[0], args[1])
+            self.__color = args[2]
+        elif len(args) == 1 and isinstance(args[0], ColorPoint):  # copy constructor
+            super().__init__(args[0].getX(), args[0].getY())
+            self.__color = args[0].__color
+
+    def read(self):
+        x, y, color = input().split()
+        super().setXY(int(x), int(y))
+        self.__color = color
+
+    def __str__(self):
+        return f"({self.getX()}, {self.getY()}): {self.__color}"
+
+    def setColor(self, color):
+        self.__color = color
+
+    def getColor(self):
+        return self.__color
