a238b6b6b8b6cb641d66e8a80ea8c0addca69d46
nguyenanhvu23707-byte <nguyenanhvu23707@gmail.com>
2026-05-15 12:57:48 +0700

    Update bài1_2.py

--- Week 9/bài1_2.py
+++ Week 9/bài1_2.py
@@ -1,23 +1,37 @@
 import math
 
-class PointTest:
-    def main(self):
-        diema=(3,4)
-        print(diema)
-        
-        xb=float(input("hoành độ điểm b: "))
-        yb=float(input("tung độ điểm b: "))
-        print("b= ",(xb, yb))
-        
-        c=(-xb, -yb)
-        print(c)
-        
-        distance_bo=math.sqrt(xb**2+yb**2)
-        print(f"khoảng cách từ b đến gốc:{distance_bo}")
-        
-        distance_ab=math.sqrt((xb-3)**2+(yb-4)**2)
-        print(f"khoảng cách từ a đến b:{distance_ab}")
-        
-if __name__ == "__main__":
-    snu=PointTest()
-    snu.main()
+class Point:
+    # Hàm khởi tạo mặc định
+    def __init__(self, x=0, y=1):
+        self.__x = x
+        self.__y = y
+
+    # Nhập tọa độ
+    def read(self):
+        self.__x, self.__y = map(int, input().split())
+
+    # Hiển thị tọa độ
+    def print(self):
+        print(f"({self.__x}, {self.__y})")
+
+    # Dời điểm
+    def move(self, dx, dy):
+        self.__x += dx
+        self.__y += dy
+
+    # Lấy hoành độ
+    def getX(self):
+        return self.__x
+
+    # Lấy tung độ
+    def getY(self):
+        return self.__y
+
+    # Khoảng cách đến gốc tọa độ hoặc điểm khác
+    def distance(self, P=None):
+        if P is None:
+            return math.sqrt(self.__x ** 2 + self.__y ** 2)
+        else:
+            dx = self.__x - P.__x
+            dy = self.__y - P.__y
+            return math.sqrt(dx ** 2 + dy ** 2)
